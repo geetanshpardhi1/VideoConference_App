0fa1b2f80bf7d002d358521d6174aa5a3d2a5863
geetanshpardhi1 <darkwolfcreation7552@gmail.com>
2024-05-04 20:21:21 +0530

    added register functionality

--- videoconference/views.py
+++ videoconference/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import RegisterForm
+
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'login.html', {'success': "Registration successful. Please login."})
+        else:
+            error_message = form.errors.as_text()
+            return render(request, 'register.html', {'error': error_message})
+
+    return render(request, 'register.html')
+
+def login_view(request):
+    if request.method=="POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/dashboard")
+        else:
+            return render(request, 'login.html', {'error': "Invalid credentials. Please try again."})
+
+    return render(request, 'login.html')
